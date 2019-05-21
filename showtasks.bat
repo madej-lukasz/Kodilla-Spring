2699ffdeca3838f7de27efaaff37257b1f27b350
madej-lukasz <madel@interia.pl>
2019-05-21 18:53:55 +0200

    Zadanie 21.4

---
+++ showtasks.bat
@@ -0,0 +1,20 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto browser
+echo.
+echo RUNCRUD has errors - breaking work
+goto fail
+
+:browser
+start http://localhost:8080/crud/v1/task/getTasks
+if "%ERRORLEVEL%" == "0" goto end
+echo.
+echo Open browser error
+goto fail
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished.
